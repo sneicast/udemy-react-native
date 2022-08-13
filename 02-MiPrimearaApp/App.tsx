bdde00c9bfc15da298cf5b58353898f69fde1889
Sneider Castillo <sneicast@hotmail.com>
2022-08-12 20:07:28 -0500

    Commit init

---
+++ 02-MiPrimearaApp/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+      }}>
+      <Text
+        style={{
+          fontSize: 55,
+        }}>
+        Hola Mundo
+      </Text>
+    </View>
+  );
+};
+
+export default App;
